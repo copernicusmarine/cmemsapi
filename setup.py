b703059a6a600423083949f04a9419eeb89c1d6d
CopernicusMarine <51791874+copernicusmarine@users.noreply.github.com>
2020-08-07 12:10:57 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ from setuptools import setup, find_packages
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
+#with open('HISTORY.rst') as history_file:
+#    history = history_file.read()
 
-requirements = [ ]
+requirements = ["fire motuclient xarray".split(' ')]
 
 setup_requirements = [ ]
 
@@ -34,7 +34,8 @@ setup(
     description="A package to help generating reliable data requests about earth observation and marine related information from Copernicus Marine Database.",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
+    long_description_content_type="text/markdown",
     include_package_data=True,
     keywords='cmemsapi',
     name='cmemsapi',
