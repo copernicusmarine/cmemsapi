303724761c5da7c96473d18e48a715b5e67e1b12
CopernicusMarine <51791874+copernicusmarine@users.noreply.github.com>
2020-08-07 11:26:13 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -83,7 +83,13 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
+html_theme_options = {
+    'repository_url': 'https://github.com/spatialaudio/nbsphinx',
+    'use_repository_button': True,
+    'use_issues_button': True,
+    'use_edit_page_button': True
+}
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
