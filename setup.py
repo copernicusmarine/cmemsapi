8e298b7827aea612dfff29dbbc66fcc06e51ed8e
Copernicus Marine <servicedesk.cmems@mercator-ocean.eu>
2020-08-08 12:33:36 +0200

    Bump version: 0.1.5 → 0.1.6

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/copernicusmarine/cmemsapi',
-    version='0.1.5',
+    version='0.1.6',
     zip_safe=False,
 	entry_points={'console_scripts':['cmemstb=cmemsapi.cmemsapi:cli']},
 )
