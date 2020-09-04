e7a821632acba1792fce171165a7c9d286c17646
Copernicus Marine <servicedesk.cmems@mercator-ocean.eu>
2020-09-04 13:46:47 +0200

    patch doc conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../cmemsapi'))
+sys.path.insert(0, os.path.abspath('..'))
 from recommonmark.parser import CommonMarkParser
 import cmemsapi
 
